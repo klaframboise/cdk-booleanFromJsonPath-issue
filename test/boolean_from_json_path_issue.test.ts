448364b680f9cd396d0adb22bd7d9b577256c8ba
Kevin Laframboise <kelaframboise@gmail.com>
2024-09-14 20:37:49 -0400

    Initial commit

---
+++ test/boolean_from_json_path_issue.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BooleanFromJsonPathIssue from '../lib/boolean_from_json_path_issue-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/boolean_from_json_path_issue-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BooleanFromJsonPathIssue.BooleanFromJsonPathIssueStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
